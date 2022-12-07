a478fbc93c0019be47e80ee946a7f5a98bb9c758
jack <jrfisch95@gmail.com>
2022-12-07 15:07:53 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+
+
+
 function App() {
+
+  const [myInput, setMyInput] = useState('');
+
+  const [foodArray, setFoodArray] = useState(['pizza', 'banana', 'apple']);
+
+  const [filteredFoodArray, setFilteredFoodArray] = useState(['pizza', 'banana', 'apple']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>Search bar:</span>
+        <input onChange={(event) => {
+          const newList = foodArray.filter(singleFood => {
+            return singleFood.toLowerCase().includes(event.target.value.toLowerCase());
+          });
+          console.log(newList);
+          setFilteredFoodArray(newList)
+        }}/>
+      </div>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        console.log('our state is ', myInput);
+        const copyFoodArray = [...foodArray];
+        copyFoodArray.push(myInput);
+        setFoodArray(copyFoodArray);
+        setFilteredFoodArray(copyFoodArray);
+        setMyInput('');
+      }}>
+        <label>Type Here:</label>
+        <input
+          type="text"
+          value={myInput}
+          onChange={(event) => {
+            console.log('it works', event.target.value)
+            setMyInput(event.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
+      <div>
+        <ol>
+          {filteredFoodArray.map(singleFood => {
+            return <li>{singleFood}</li>
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
